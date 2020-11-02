5d57a1b2e905fd404e849798cf8b2e38f33d5722
Edmundo <jtvedy@gmail.com>
2020-11-02 17:56:06 -0300

    mudando nome de algumas variaeis para fazer mais ssentido e definindo a constante: SIZE

--- monitor.h
+++ monitor.h
@@ -8,23 +8,24 @@
 #define LDR 0
 #define IRRIGATION 1
 
+#define SIZE 2
 
-SemaphoreHandle_t mutex[2] = {xSemaphoreCreateMutex(), xSemaphoreCreateMutex()};
+SemaphoreHandle_t mutex[SIZE] = {xSemaphoreCreateMutex(), xSemaphoreCreateMutex()};
+static uint8_t secureValues[SIZE] = {0, 0};
 
-static uint8_t sensorReading[2] = {0, 0};
 
-
-void set_value(uint8_t value, uint8_t sensor){
-  xSemaphoreTake( mutex[sensor], portMAX_DELAY );
-  sensorReading[sensor] = value;
-  xSemaphoreGive(mutex[sensor]);
+void set_value(uint8_t value, uint8_t variable){
+  xSemaphoreTake( mutex[variable], portMAX_DELAY );
+  secureValues[variable] = value;
+  xSemaphoreGive(mutex[variable]);
 }
 
-uint8_t get_value( uint8_t sensor){
+
+uint8_t get_value( uint8_t variable){
   uint8_t aux;
-  xSemaphoreTake( mutex[sensor], portMAX_DELAY );
-  aux = sensorReading[sensor];
-  xSemaphoreGive(mutex[sensor]);
+  xSemaphoreTake( mutex[variable], portMAX_DELAY );
+  aux = secureValues[variable];
+  xSemaphoreGive(mutex[variable]);
   return aux;
 }
 
