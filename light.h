db7155b7fd5c102a47f411bac4f85f45378c582e
Edmundo <jtvedy@gmail.com>
2020-11-10 11:36:55 -0300

    muitos comentarios explicando as coisas

--- light.h
+++ light.h
@@ -6,17 +6,20 @@
 #include <timers.h>
 #include <queue.h>
 
-#define LIGHTPIN 5
-#define DARK 0
-#define LIGHT 1
+#define LIGHTPIN 5 // pino onde ficará o rele/led
+#define DARK 0 // posição no vetor para o tempo de escuro
+#define LIGHT 1 // posição no vetor para o tempo de claro
 
-TimerHandle_t light_timer;
-bool enableLight = false;
-bool current_light_state = DARK;
-QueueHandle_t enable_disable_Q = xQueueCreate(1, sizeof(bool));
-uint16_t times[2] = { pdMS_TO_TICKS(500), pdMS_TO_TICKS(2000)};
+uint16_t times[2] = { pdMS_TO_TICKS(500), pdMS_TO_TICKS(2000)}; // tempos em cada estado
+TimerHandle_t light_timer; // timer do sistema 
+
+bool enableLight = false; // status se o sistema está ativo ou não
+bool current_light_state = DARK; // estado atual do sistema (CLARO OU ESCURO)
+QueueHandle_t enable_disable_Q = xQueueCreate(1, sizeof(bool)); // fila de comunicação para atiação e desatiação do sistema
 
 
+//fuções para ficar mais legiel quando estiver pedindo para ligar ou desligar a luz
+//***Talvez tenha que adicionar ao monitor se o ldr tbm for usar isso... 
 void light_turn_on(){
     digitalWrite(LIGHTPIN, HIGH);
 }
@@ -25,23 +28,27 @@ void light_turn_off(){
     digitalWrite(LIGHTPIN, LOW);
 }
 
+/* 
+  Alterar o estado atual para o seguinte. 
+  Mudar o valor do timer para o valor do estado correspondente
+*/
 void light_state_toggle(TimerHandle_t xTimer){
-  digitalWrite(LIGHTPIN, !current_light_state);
-  current_light_state = !current_light_state;
+  digitalWrite(LIGHTPIN, !current_light_state); // ***mudar para uma  função do monitor se o ldr tbm for usar
+  current_light_state = !current_light_state; //alterando o estado atual
   Serial.print("changing current_light_state to: ");
   Serial.println(current_light_state ? "LIGHT" : "DARK");
-  xTimerChangePeriod(xTimer, times[current_light_state], 0);
+  xTimerChangePeriod(xTimer, times[current_light_state], 0);//alterar valor do timer
 }
 
 
 /*
-Task que espera uma interrupção colocar um valor na fila
-quando isso ocorre olha esse valor e desabilita ou habilita o timer
+Task que espera uma interrupção colocar um valor na fila.
+Quando isso ocorre ela olha esse valor e desabilita ou habilita o timer
 */
-
 void light_enable_disable( void *pv){
-  bool L;
+  bool L; //buffer para guardar valor que vem da fila
   for(;;){
+    //Aguardar receber conteudo da fila. Então salvar esse conteudo na variavel L
     if ( xQueueReceive( enable_disable_Q, &L, portMAX_DELAY) == pdPASS) {
       
       if(L){
@@ -50,28 +57,29 @@ void light_enable_disable( void *pv){
       }
       else{
         xTimerStop( light_timer, 0 );
-        digitalWrite(LIGHTPIN, LOW);
+        digitalWrite(LIGHTPIN, LOW); //*** mudar para monitor 
         Serial.println("Parou o timer");
       }
     }
   }
 }
 
-
+// estrutura do valor que dee ser passado para a fila de mudaça do valor dos tempos de claro e escuro
 struct changeValueData {
-  bool alter_state;
-  uint16_t newValue;
+  bool alter_state; // estado sendo alterado
+  uint16_t newValue; // noo valor
 };
 
 void light_change_value(void *pv){
   
-  struct changeValueData* data;
+  struct changeValueData* data; //buffer para guardar valor que vem da fila
 
   for(;;){
+    //***Tem que ser outra fila e não a mesma que ta usando para desabilitar e habilitar o timer
     if( xQueueReceive( enable_disable_Q, data, portMAX_DELAY) == pdPASS ){
-      times[data->alter_state] = data->newValue;
+      times[data->alter_state] = data->newValue; //mudando valor no vetor
       if(current_light_state == data->alter_state){
-         xTimerChangePeriod( light_timer, pdMS_TO_TICKS(data->newValue), 0 );
+         xTimerChangePeriod( light_timer, pdMS_TO_TICKS(data->newValue), 0 ); //se o alor sendo mudado é o do estado atual, alterar tbm o timer
       }
     }
   }
@@ -89,12 +97,12 @@ void light_setup(){
     ); 
 
     xTaskCreate(
-        light_enable_disable, // Task function
-        "light_start_stop", // Task name for humans
-        128, 
-        NULL, // Task parameter
-        1, // Task priority
-        NULL
+        light_enable_disable, // implementação da task
+        "light_start_stop", // nome para debug
+        128, // espaço reservado na memoria
+        NULL, // parametro para ser passado para task 
+        1, // prioridade da task
+        NULL //handler para manipulação da task
     );
 }
 
